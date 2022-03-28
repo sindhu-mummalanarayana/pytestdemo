9730e3fb746302fb5ec5609422175f7dc164b7c4
sindhu <sindhu.mummalanarayana@gmail.com>
2022-03-28 17:47:40 -0500

    conftest and fixture usage tries to run single TC using 2 fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest as pytest
+from selenium import webdriver
+@pytest.fixture(scope='class')
+def setup(request):
+    global driver
+    driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+    driver.get('https://accounts.shopify.com/signup?rid=dbc83c4e-6fa8-45fb-a6c3-af6963dc6515')
+    driver.maximize_window()
+    request.cls.driver=driver
+    yield
+    driver.close()
+
+
+
+@pytest.fixture(scope='class')
+def testdata():
+    return ("Sindhu","MN","sindhu.suprith@gmail.com","dummy1234@@","dummy1234@@")
---
+++ tests/test_demo1.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+@pytest.mark.usefixtures("setup")
+class Test0ne:
+    def test_TC01_ShopifyHomepage_LoginButton(self):
+
